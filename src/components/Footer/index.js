90fb75e624f88b9f39e8f1ad2f26cfd4ec76c097
VIN1332-John <john.arockiasamy@verticurl.com>
2021-09-07 13:49:02 +0530

    Code reverted in Footer and Tiles card

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,12 +13,12 @@ const Footer = (props) => {
   const links = props?.fields?.Links;
   const SocialAccountHeading = props?.fields?.SocialAccountHeading;
   const SocialAccounts = props?.fields?.SocialAccount;
-  // const convertedSocial = SocialAccounts.map((social) => ({
-  //   label: social.displayName,
-  //   service: social.fields.service.value,
-  //   url: social.fields.URL.value.href,
-  // }));
-  // console.log(convertedSocial);
+  const convertedSocial = SocialAccounts.map((social) => ({
+    label: social.displayName,
+    service: social.fields.service.value,
+    url: social.fields.URL.value.href,
+  }));
+  console.log(convertedSocial);
   return (
     <div className="c-footer-address">
       <div className="c-footer-address__container">
