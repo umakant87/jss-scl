98c5862db51127ec1037d86cf2c55ee104d35c79
umakant <umakanth.shivabal@verticurl.com>
2021-09-21 14:43:59 +0530

    tab

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Text } from '@sitecore-jss/sitecore-jss-react';
+import { Text, RichText } from '@sitecore-jss/sitecore-jss-react';
 
-const Tab = ({ props }) => {
-  const fields = props?.fields.Tab;
-  const [activeTab, setaActiveTab] = useState(fields[0].fields.Title);
+const Tab = (props) => {
+  const tabs = props?.fields?.Tab;
+  const [activeTab, setaActiveTab] = useState(tabs[0].fields.Title.value);
   const handleClick = (e, newActiveTab) => {
     e.preventDefault();
     setaActiveTab(newActiveTab);
@@ -11,12 +11,12 @@ const Tab = ({ props }) => {
   return (
     <div className="c-tab">
       <ul className="nav nav-tabs" id="myTab">
-        {fields &&
-          fields.map((tab) => {
-            const label = tab.fields.Title;
+        {tabs &&
+          tabs.map((tab, index) => {
+            const label = tab.fields.Title.value;
             console.log(label);
             return (
-              <li className="nav-item" key={label}>
+              <li className="nav-item" key={`title_${index}`}>
                 <a
                   data-toggle="tab"
                   className={[label === activeTab ? 'active' : '', 'nav-link'].join(' ')}
@@ -29,13 +29,13 @@ const Tab = ({ props }) => {
           })}
       </ul>
       <div className="tab-content my-tab">
-        {fields &&
-          fields.map((content) => {
-            if (content.fields.Title === activeTab) {
+        {tabs &&
+          tabs.map((content, index) => {
+            if (content.fields.Title.value === activeTab) {
               return (
-                <div key={content.fields.Title} role="tabpanel" className="tab-pane active">
-                  <h2>{content.fields.Title}</h2>
-                  <div>{content.fields.Description}</div>
+                <div key={index} role="tabpanel" className="tab-pane active">
+                  <Text type="h2" field={content.fields.Title} />
+                  <RichText type="p" field={content.fields.Description} />
                 </div>
               );
             }
--- src/components/Tab/mock.js
+++ src/components/Tab/mock.js
@@ -1,19 +1,51 @@
-const fields = [
-  {
-    label: 'Tab1',
-    content:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. <i>Lorem Ipsum has been the industrys</i> standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-  },
-  {
-    label: 'Tab2',
-    content:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. <i>Lorem Ipsum has been the industrys</i> standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-  },
-  {
-    label: 'Tab3',
-    content:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. <i>Lorem Ipsum has been the industrys</i> standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
-  },
-];
+const fields = {
+  Tab: [
+    {
+      id: '51a42628-c1d1-474d-bd1e-543df9ef296d',
+      url: '/Data/Tab/MainTabs/Tab1',
+      name: 'Tab1',
+      displayName: 'Tab1',
+      fields: {
+        Description: {
+          value:
+            '<span style="background-color: #ffffff; color: #333333;">Java is a programming language and computing platform first released by Sun Microsystems in 1995. There are lots of applications and websites that will not work unless you have Java installed, and more are created every day. Java is fast, secure, and reliable. From laptops to datacenters, game consoles to scientific supercomputers, cell phones to the Internet, Java is everywhere!</span>',
+        },
+        Title: {
+          value: 'What is Java technology ?',
+        },
+      },
+    },
+    {
+      id: '6f6cf725-b2d2-49ef-8db9-5bf291362c0c',
+      url: '/Data/Tab/MainTabs/Tab2',
+      name: 'Tab2',
+      displayName: 'Tab2',
+      fields: {
+        Description: {
+          value:
+            '<span style="background-color: #ffffff; color: #4d5156;">A content management system is a computer software used to manage the creation and modification of digital content. A CMS is typically used for enterprise content management and web content management.</span>',
+        },
+        Title: {
+          value: 'What is Content Management System ?',
+        },
+      },
+    },
+    {
+      id: 'cfb88b6d-9f91-4602-8a94-29235d3b2a67',
+      url: '/Data/Tab/MainTabs/Tab3',
+      name: 'Tab3',
+      displayName: 'Tab3',
+      fields: {
+        Description: {
+          value:
+            '<span style="background-color: #ffffff; color: #4d5156;">The .NET Framework is a software framework developed by Microsoft that runs primarily on Microsoft Windows. It includes a large class library called Framework Class Library and provides language interoperability across several programming languages.</span>',
+        },
+        Title: {
+          value: 'What is .NET Framework?',
+        },
+      },
+    },
+  ],
+};
 
 export default fields;
