93c1ae64b1d64ecc7ae2f3cf11d90b9933d33db9
VIN1332-John <john.arockiasamy@verticurl.com>
2021-09-21 12:19:34 +0530

    Minor tweaks in Youtube and Button components

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -12,6 +12,7 @@ export const Button = (props) => {
   const size = props?.fields ? props?.fields?.size?.value : props.size;
   const label = props?.fields ? props?.fields?.label?.value : props.label;
   const callToAction = props?.fields ? props?.fields?.callToAction?.value : props.callToAction;
+  const onClick = props.onClick ? props.onClick : '';
   const CTA = (link) => {
     window.open(link, '_blank');
   };
@@ -21,6 +22,7 @@ export const Button = (props) => {
       className={['c-button', `c-button__${size}`, `c-button__${className}`].join(' ')}
       style={backgroundColor && { backgroundColor }}
       {...(callToAction && { onClick: () => CTA(callToAction) })}
+      {...(onClick && { onClick: onClick })}
     >
       {label}
     </button>
